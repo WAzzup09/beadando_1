4ecb11f7f5bd531d2fde9984189e3a3f164677f7
WAzzup09 <weiszandras07@gmail.com>
2024-12-04 10:23:44 +0100

    háromszög hozzáadva main.py-hoz

--- main.py
+++ main.py
@@ -1,7 +1,33 @@
+#ASCII képek generálása
+
 import random
-import fuggvenyek
+import os
 
+def menu():
+    os.system('cls')
 
-szamok = fuggvenyek.intgen(1,100000000,100000)
+    rajzok = ["(0) Kilépés", "(1) Háromszög", ""]
+    art = -1
 
-print(szamok)
+    while art != 0:
+        for i in range(len(rajzok)):
+            print(rajzok[i])
+
+        art = int(input("Kérem adja meg, melyik ASCII rajzot szeretné megtekinteni: "))
+
+        if art == 0:
+            print("KIléptél.")
+            return
+        elif art == 1:
+            haromszog(6)
+        
+        
+
+def haromszog(n):
+    minta = ["*"]
+    for i in range(n):
+        szokoz = " " * (2**i)
+        minta = [szokoz + x + szokoz for x in minta] + [x + " " + x for x in minta]
+    print("\n".join(minta))
+
+menu()

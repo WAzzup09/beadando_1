9971b01b732b27c5694c56327286e2020f397daa
WAzzup09 <weiszandras07@gmail.com>
2024-12-06 21:42:15 +0100

    négyszög hozzáadva

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import os
 def menu():
     os.system('cls')
 
-    rajzok = ["(0) Kilépés", "(1) Háromszög"]
+    rajzok = ["(0) Kilépés", "(1) Háromszög", "(2) Négyzet"]
     art = -1
 
     while art != 0:
@@ -20,14 +20,24 @@ def menu():
             return
         elif art == 1:
             haromszog(6)
+        elif art == 2:
+            negyszog(4)
         
         
-
-def haromszog(n):
-    minta = ["▲"]
+# Sierpiński-háromszög
+def haromszog(n:int):
+    minta = ["*"]
     for i in range(n):
         szokoz = " " * (2**i)
         minta = [szokoz + x + szokoz for x in minta] + [x + " " + x for x in minta]
     print("\n".join(minta))
+
+# Sierpiński-szőnyeg
+def negyszog(n):
+    minta = ["#"]
+    for i in range(n):
+        minta = [x + x + x for x in minta] + [x + x.replace("#", " ")+ x for x in minta] + [x + x + x for x in minta]
+    print("\n".join(minta))
+
 
 menu()

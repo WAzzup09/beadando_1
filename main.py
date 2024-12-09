9e41082da4ff279f6758a3c720cd096a444a06e1
WAzzup09 <weiszandras07@gmail.com>
2024-12-09 11:30:46 +0100

    main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import os
 def menu():
     os.system('cls')
 
-    rajzok = ["(0) Kilépés", "(1) Háromszög", "(2) Négyzet"]
+    rajzok = ["(0) Kilépés", "(1) Háromszög", "(2) Négyzet", "(3) Fenyőfa"]
     art = -1
 
     while art != 0:
@@ -22,6 +22,10 @@ def menu():
             haromszog(6)
         elif art == 2:
             negyszog(4)
+        elif art == 3:
+            karifa(75)
+        elif art == 4:
+            csillag(7)
         
         
 # Sierpiński-háromszög
@@ -39,5 +43,19 @@ def negyszog(n):
         minta = [x + x + x for x in minta] + [x + x.replace("#", " ")+ x for x in minta] + [x + x + x for x in minta]
     print("\n".join(minta))
 
+# Fenyőfa
+def karifa(n):
+    for i in range(n + 1):
+        print(' '*(n-i), '*'*((2*i)-1))
+
+    for i in range(5):
+        print(' '*(n-3), '*'*(3))
+
+# Csillag
+def csillag(n):
+    for i in range(n-2):
+        print(' '*(n-i), '*'*((2*i)-1))
+    for i in range(n):
+        print('*'*(n+i),' '*(i-1),)
 
 menu()
